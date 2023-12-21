9565018548764aaa558f50d7e369515e0ba5678b
Efekan.Gvndk <e.gvndk00@gmail.com>
2023-12-21 21:14:12 +0300

    Initial Commit

---
+++ New Class Memorize/New_Class_MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_Class_MemorizeApp.swift
+//  New Class Memorize
+//
+//  Created by Efekan Güvendik on 21.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct New_Class_MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
