7c7732134432b71d371ca0e9ca485683681557db
Efekan.Gvndk <e.gvndk00@gmail.com>
2024-01-04 15:50:00 +0300

    Swift UI
    Learning New Methods

--- New Class Memorize/ContentView.swift
+++ New Class Memorize/ContentView.swift
@@ -9,12 +9,81 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            HStack{
+                ZStack (content: { //ZStack olursa iç içe Vstack olursa alt üst HStack olursa yan yana
+                      //burdan sonra eğer imagename dersek Assetlerden bir görünüm alır ve öyle gelir
+                    RoundedRectangle(cornerRadius: 50)
+                        .foregroundColor(.white)
+                    RoundedRectangle(cornerRadius: 50)
+                        .strokeBorder(style : StrokeStyle (lineWidth: 5, dash: [10,1] ))
+                    Text("💩").font(.largeTitle)
+                })
+                ZStack (content: { //ZStack olursa iç içe Vstack olursa alt üst HStack olursa yan yana
+                    //burdan sonra eğer imagename dersek Assetlerden bir görünüm alır ve öyle gelir
+                  RoundedRectangle(cornerRadius: 50)
+                      .foregroundColor(.white)
+                  RoundedRectangle(cornerRadius: 50)
+                      .strokeBorder(style : StrokeStyle (lineWidth: 5, dash: [10,1] ))
+                  Text("💩").font(.largeTitle)
+              })
+                ZStack (content: { //ZStack olursa iç içe Vstack olursa alt üst HStack olursa yan yana
+                    //burdan sonra eğer imagename dersek Assetlerden bir görünüm alır ve öyle gelir
+                  RoundedRectangle(cornerRadius: 50)
+                      .foregroundColor(.white)
+                  RoundedRectangle(cornerRadius: 50)
+                      .strokeBorder(style : StrokeStyle (lineWidth: 5, dash: [10,1] ))
+                  Text("💩").font(.largeTitle)
+              })
+                ZStack (content: { //ZStack olursa iç içe Vstack olursa alt üst HStack olursa yan yana
+                    //burdan sonra eğer imagename dersek Assetlerden bir görünüm alır ve öyle gelir
+                  RoundedRectangle(cornerRadius: 50)
+                      .foregroundColor(.white)
+                  RoundedRectangle(cornerRadius: 50)
+                      .strokeBorder(style : StrokeStyle (lineWidth: 5, dash: [10,1] ))
+                  Text("💩").font(.largeTitle)
+              })
+            }
+            HStack{
+                ZStack (content: { //ZStack olursa iç içe Vstack olursa alt üst HStack olursa yan yana
+                      //burdan sonra eğer imagename dersek Assetlerden bir görünüm alır ve öyle gelir
+                    RoundedRectangle(cornerRadius: 50)
+                        .foregroundColor(.white)
+                    RoundedRectangle(cornerRadius: 50)
+                        .strokeBorder(style : StrokeStyle (lineWidth: 5, dash: [10,1] ))
+                    Text("💩").font(.largeTitle)
+                })
+                ZStack (content: { //ZStack olursa iç içe Vstack olursa alt üst HStack olursa yan yana
+                    //burdan sonra eğer imagename dersek Assetlerden bir görünüm alır ve öyle gelir
+                  RoundedRectangle(cornerRadius: 50)
+                      .foregroundColor(.white)
+                  RoundedRectangle(cornerRadius: 50)
+                      .strokeBorder(style : StrokeStyle (lineWidth: 5, dash: [10,1] ))
+                  Text("💩").font(.largeTitle)
+              })
+                ZStack (content: { //ZStack olursa iç içe Vstack olursa alt üst HStack olursa yan yana
+                    //burdan sonra eğer imagename dersek Assetlerden bir görünüm alır ve öyle gelir
+                  RoundedRectangle(cornerRadius: 50)
+                      .foregroundColor(.white)
+                  RoundedRectangle(cornerRadius: 50)
+                      .strokeBorder(style : StrokeStyle (lineWidth: 5, dash: [10,1] ))
+                  Text("💩").font(.largeTitle)
+              })
+                ZStack (content: { //ZStack olursa iç içe Vstack olursa alt üst HStack olursa yan yana
+                    //burdan sonra eğer imagename dersek Assetlerden bir görünüm alır ve öyle gelir
+                  RoundedRectangle(cornerRadius: 50)
+                      .foregroundColor(.white)
+                  RoundedRectangle(cornerRadius: 50)
+                      .strokeBorder(style : StrokeStyle (lineWidth: 5, dash: [10,1] ))
+                  Text("💩").font(.largeTitle)
+              })
+            }
         }
+        
+        
+        
+        .foregroundColor(.blue)
+        .imageScale(.small)
         .padding()
     }
 }
