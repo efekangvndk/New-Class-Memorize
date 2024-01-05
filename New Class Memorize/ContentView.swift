113e96e6481d105a44ce95552e9bfe429e385f1d
Efekan.Gvndk <e.gvndk00@gmail.com>
2024-01-06 01:04:10 +0300

    Update ContentView.swift
    New design and new informations

--- New Class Memorize/ContentView.swift
+++ New Class Memorize/ContentView.swift
@@ -7,34 +7,44 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
+struct ContentView: View {         //Struck yapı elemanları için kullanırız.
+    var body: some View {          // body : bir view yaptığımız onun vicudu gibi düşünürüz.
         VStack{
             HStack{
                 
-                CardView(isFaceUp: false)
+                CardView(isFaceUp: false)                      // Tanımladığımız değişkenleri buraya yazdık kısa bir func.
                 CardView(isFaceUp: true)
                 CardView(isFaceUp: false)
                 
             }
             
         }
-        .foregroundColor(.gray)
-        .padding()
+        .foregroundColor(.black)
+        .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 8))                   // Boşluk bırakmak ve detay.
     }
 }
 
+                                        /// --- Öğrenim herne oluşturduysak onun modifiye için altına uygun methodu yazmak yeterli.
+
+
+
 struct CardView : View {
     
-    var isFaceUp : Bool = false
+    var isFaceUp : Bool = false         // Bool : true veya false için bir parametredir. Bu şekilde kısa yoldan if else yerine kullanarız.
     var body: some View {
-        ZStack{                      // İç İçe demek   <----------->   Ek olarak Hstack de yan yana demek.
+        ZStack{                         // İç İçe demek   <----------->   Ek olarak Hstack de yan yana demek.
             if isFaceUp {                                       // Bu karşmaşık olay bize mi dönük yoksa arkası mı dönük anlamındadır.
                 ZStack(alignment: .top, content: {              // Üstlü altı demek.
                     RoundedRectangle(cornerRadius: 50)          // Kenear Yuvarlaklığı için
+                        .fill(LinearGradient(                   // Cisimler için (kare daire v.b)içini doldur demek
+                            gradient: Gradient(colors: [.red, .pink, .purple,.white, .white, ]), // Bir alanın türevi anlamında gelir hafif geçiş
+                            startPoint: .bottom,                                                 // Yaparken işimize yara.
+                            endPoint: .top
+                                            ))
                         .foregroundColor(.black)                // Arkaplan rengi
                     Text("Hello").font(.largeTitle)             // .font size ayarı
-                        .background(RoundedRectangle(cornerRadius: 10).foregroundColor(.white))   //Hello yazısının arkasına koyulmuş bir background.
+                        .foregroundColor(.pink)
+                        .background(RoundedRectangle(cornerRadius: 15).foregroundColor(.white))
                     RoundedRectangle(cornerRadius: 50)
                         .strokeBorder(style : StrokeStyle (lineWidth: 5, dash: [10,1] ))          // Açılan küçük çizgi ayarları.
                     
@@ -43,12 +53,14 @@ struct CardView : View {
             }else{
                 ZStack(alignment: .center, content :{
                     RoundedRectangle(cornerRadius: 50)
-                        .foregroundColor(.yellow)                // Burada kullandığımız foregroundColor text gibi cisimleri alır
+                        .foregroundColor(.purple)                // Burada kullandığımız foregroundColor text gibi cisimleri alır
                     Text("Merhaba").font(.title)                 // Bunun yerine backGrounc kullanırsak direk arka plana demiş olur.
+                        .foregroundColor(.white)
                         .rotationEffect(Angle(degrees: 50))
-                        .background(RoundedRectangle(cornerRadius: 100).foregroundColor(.black)) // yazının arka planı gibi düşünülür. ve aynı zaman eğimli
+                        .background(RoundedRectangle(cornerRadius: 100).foregroundColor(.black)) // yazının arka planı düşünülür. ve aynı zaman eğimli
                     RoundedRectangle(cornerRadius: 50)
                         .strokeBorder(style : StrokeStyle (lineWidth: 5, dash: [10,1] ))
+                        .foregroundColor(.red)
                 })
             }
         }
@@ -57,6 +69,6 @@ struct CardView : View {
 
 
 
-#Preview {
+#Preview {            // Ön izleme teknik açıdan Canvas
     ContentView()
 }
