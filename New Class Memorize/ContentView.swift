7ad9bf2af9cf488ac7fdb55e7a5e851550b1c1f8
Efekan.Gvndk <e.gvndk00@gmail.com>
2024-01-06 00:06:15 +0300

    Update ContentView.swift
    Learing too

--- New Class Memorize/ContentView.swift
+++ New Class Memorize/ContentView.swift
@@ -11,13 +11,13 @@ struct ContentView: View {
     var body: some View {
         VStack{
             HStack{
+                
                 CardView(isFaceUp: false)
                 CardView(isFaceUp: true)
-                CardView()
+                CardView(isFaceUp: false)
                 
-             
             }
-  
+            
         }
         .foregroundColor(.gray)
         .padding()
@@ -28,27 +28,30 @@ struct CardView : View {
     
     var isFaceUp : Bool = false
     var body: some View {
-            ZStack(alignment: .top, content: {   // İç İçe demek                 Ek olarak Hstack de yan yana demek.
-                if isFaceUp {           // Bu karşmaşık olay bize mi dönük yoksa arkası mı dönük anlamındadır.
-                VStack{         // Üstlü altı demek.
+        ZStack{                      // İç İçe demek   <----------->   Ek olarak Hstack de yan yana demek.
+            if isFaceUp {                                       // Bu karşmaşık olay bize mi dönük yoksa arkası mı dönük anlamındadır.
+                ZStack(alignment: .top, content: {              // Üstlü altı demek.
                     RoundedRectangle(cornerRadius: 50)          // Kenear Yuvarlaklığı için
                         .foregroundColor(.black)                // Arkaplan rengi
                     Text("Hello").font(.largeTitle)             // .font size ayarı
+                        .background(RoundedRectangle(cornerRadius: 10).foregroundColor(.white))   //Hello yazısının arkasına koyulmuş bir background.
                     RoundedRectangle(cornerRadius: 50)
-                        .strokeBorder(style : StrokeStyle (lineWidth: 5, dash: [10,1] ))   // Açılan küçük çizgi ayarları.
+                        .strokeBorder(style : StrokeStyle (lineWidth: 5, dash: [10,1] ))          // Açılan küçük çizgi ayarları.
                     
-                }
-            
-                }else{
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 50)
-                            .foregroundColor(.yellow)
-                        Text("Merhaba").font(.title)
-                        RoundedRectangle(cornerRadius: 50)
-                            .strokeBorder(style : StrokeStyle (lineWidth: 5, dash: [10,1] ))
-                }
+                })
+                
+            }else{
+                ZStack(alignment: .center, content :{
+                    RoundedRectangle(cornerRadius: 50)
+                        .foregroundColor(.yellow)                // Burada kullandığımız foregroundColor text gibi cisimleri alır
+                    Text("Merhaba").font(.title)                 // Bunun yerine backGrounc kullanırsak direk arka plana demiş olur.
+                        .rotationEffect(Angle(degrees: 50))
+                        .background(RoundedRectangle(cornerRadius: 100).foregroundColor(.black)) // yazının arka planı gibi düşünülür. ve aynı zaman eğimli
+                    RoundedRectangle(cornerRadius: 50)
+                        .strokeBorder(style : StrokeStyle (lineWidth: 5, dash: [10,1] ))
+                })
             }
-        })
+        }
     }
 }
 
