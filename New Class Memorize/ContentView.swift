937378e937a885b049c0e1a60edc2b1af23a4ffe
Efekan.Gvndk <e.gvndk00@gmail.com>
2024-01-05 19:55:07 +0300

    Update ContentView.swift
    Review the code block comment lines and new components.

--- New Class Memorize/ContentView.swift
+++ New Class Memorize/ContentView.swift
@@ -11,12 +11,15 @@ struct ContentView: View {
     var body: some View {
         VStack{
             HStack{
+                CardView(isFaceUp: false)
+                CardView(isFaceUp: true)
                 CardView()
+                
              
             }
   
         }
-        .foregroundColor(.blue)
+        .foregroundColor(.gray)
         .padding()
     }
 }
@@ -25,18 +28,26 @@ struct CardView : View {
     
     var isFaceUp : Bool = false
     var body: some View {
-        HStack(content: {
-              
-                       
+            ZStack(alignment: .top, content: {   // İç İçe demek                 Ek olarak Hstack de yan yana demek.
+                if isFaceUp {           // Bu karşmaşık olay bize mi dönük yoksa arkası mı dönük anlamındadır.
+                VStack{         // Üstlü altı demek.
+                    RoundedRectangle(cornerRadius: 50)          // Kenear Yuvarlaklığı için
+                        .foregroundColor(.black)                // Arkaplan rengi
+                    Text("Hello").font(.largeTitle)             // .font size ayarı
                     RoundedRectangle(cornerRadius: 50)
-                        .foregroundColor(.black)
-                    RoundedRectangle(cornerRadius: 50)
-                        .strokeBorder(style : StrokeStyle (lineWidth: 5, dash: [10,1] ))
-            Text("Hello").font(.largeTitle)
-                
+                        .strokeBorder(style : StrokeStyle (lineWidth: 5, dash: [10,1] ))   // Açılan küçük çizgi ayarları.
                     
- 
-        
+                }
+            
+                }else{
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 50)
+                            .foregroundColor(.yellow)
+                        Text("Merhaba").font(.title)
+                        RoundedRectangle(cornerRadius: 50)
+                            .strokeBorder(style : StrokeStyle (lineWidth: 5, dash: [10,1] ))
+                }
+            }
         })
     }
 }
