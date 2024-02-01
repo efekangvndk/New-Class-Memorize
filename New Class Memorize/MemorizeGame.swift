a208df1950ceee9bb9244d5ea1a669e95e71d6f2
Efekan.Gvndk <e.gvndk00@gmail.com>
2024-02-01 12:41:17 +0300

    memoryGame

---
+++ New Class Memorize/MemorizeGame.swift
@@ -0,0 +1,20 @@
+//
+//  MemorizeGame.swift
+//  New Class Memorize
+//
+//  Created by Efekan Güvendik on 1.02.2024.
+//
+
+import Foundation
+
+struct MemoryGame {
+    
+    var cards : Array<Cards>
+    
+    func choose(card:Cards){
+        
+    }
+    struct Cards{
+        
+    }
+}
