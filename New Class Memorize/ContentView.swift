61f2186ea148a612bc27b080d0be6b05148b235d
Efekan.Gvndk <e.gvndk00@gmail.com>
2024-02-03 22:59:11 +0300

    fix

--- New Class Memorize/ContentView.swift
+++ New Class Memorize/ContentView.swift
@@ -130,7 +130,7 @@ struct CardView : View {
                     
                     RoundedRectangle(cornerRadius: 50)
                         .foregroundColor(.purple)                    // Burada kullandığımız foregroundColor text gibi cisimleri alır
-                    Text("Hİ").font(.title)                          // Bunun yerine backGrounc kullanırsak direk arka plana demiş olur.
+                    Text("Hİi").font(.title)                          // Bunun yerine backGrounc kullanırsak direk arka plana demiş olur.
                         .foregroundColor(.white)
                         .rotationEffect(Angle(degrees: 90))
                         .background(RoundedRectangle(cornerRadius: 100).foregroundColor(.black))  // yazının arka planı düşünülür. ve aynı zaman eğimli
