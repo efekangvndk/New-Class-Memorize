b850ec0f64146ae10973eae2cfbdf692760b1211
Efekan.Gvndk <e.gvndk00@gmail.com>
2024-02-03 22:58:22 +0300

    fix1

--- New Class Memorize/ContentView.swift
+++ New Class Memorize/ContentView.swift
@@ -116,7 +116,7 @@ struct CardView : View {
                         .foregroundColor(.black)                      // Arkaplan rengi
                     VStack{
                         Text(content2).font(.largeTitle)          // .font size ayarı
-                        Text("bo").font(.title3)
+                        Text("boo").font(.title3)
                             .foregroundColor(.black)
                         
                     }
